f4d0d5e9e532697dca679bb320d5921cf6c08659
fishingboy <fishingboy@gmail.com>
2024-08-21 16:39:44 +0800

    泛型函式

---
+++ test/Exercise_List.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
+
+func (l *List[T]) Append(value T) *List[T] {
+	newNode := &List[T]{val: value}
+	if l == nil {
+		return newNode
+	}
+
+	current := l
+	for current.next != nil {
+		current = current.next
+	}
+	current.next = newNode
+	return l
+}
+
+func (l *List[T]) PrintAll() {
+	current := l
+	for current != nil {
+		fmt.Println(">", current.val)
+		current = current.next
+	}
+}
+
+func main() {
+	// 使用 * 號
+	var list *List[int]
+	list = list.Append(10)
+	list = list.Append(20)
+	list = list.Append(30)
+	list.PrintAll()
+
+	// 不使用 * 號
+	var list2 = List[string]{val: "s"}
+	list2.Append("Leo")
+	list2.Append("Evonne")
+	list2.Append("Mou")
+	list2.PrintAll()
+
+}
