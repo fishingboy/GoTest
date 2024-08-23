bf063fd9187a3bc30111bf4e162fe662571d91ff
fishingboy <fishingboy@gmail.com>
2024-08-23 16:14:02 +0800

    測試延長 redis 的 expired time

--- test/redis.go
+++ test/redis.go
@@ -16,16 +16,16 @@ func main() {
 		DB:       0,  // use default DB
 	})
 
-	err := rdb.Set(ctx, "key", "value", 600*time.Second).Err()
+	err := rdb.Set(ctx, "mykey", "value", 30*time.Second).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := rdb.Get(ctx, "key").Result()
+	val, err := rdb.Get(ctx, "mykey").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("key", val)
+	fmt.Println("mykey", val)
 
 	val2, err := rdb.Get(ctx, "productStat").Result()
 	if err == redis.Nil {
@@ -35,6 +35,32 @@ func main() {
 	} else {
 		fmt.Println("key2", val2)
 	}
+
+	// 檢查剩餘的過期時間
+	ttl, err := rdb.TTL(ctx, "mykey").Result()
+	if err != nil {
+		fmt.Println("Failed to get TTL:", err)
+		return
+	}
+
+	for {
+		time.Sleep(10 * time.Second)
+		// 延長過期時間，增加 20 秒
+		err = rdb.Expire(ctx, "mykey", 30*time.Second).Err()
+		if err != nil {
+			fmt.Println("Failed to extend expiration:", err)
+			return
+		}
+
+		// 再次檢查剩餘的過期時間
+		ttl, err = rdb.TTL(ctx, "mykey").Result()
+		if err != nil {
+			fmt.Println("Failed to get TTL:", err)
+			return
+		}
+		fmt.Printf("Extended TTL: %v\n", ttl)
+	}
+
 	// Output: key value
 	// key2 does not exist
 }
